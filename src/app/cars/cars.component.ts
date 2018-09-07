5ce16995e36cd734affbe73628e8827b0e9a2b05
Valeriy_V_Bagaev <v.bagaev@sarrc.ru>
2018-09-07 10:55:30 +0100

    Область видимости CSS

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -1,13 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-cars',
   templateUrl: './cars.component.html',
-  styleUrls: ['./cars.component.css']
+  styleUrls: ['./cars.component.css'],
 })
 export class CarsComponent {
-
-
 
   cars: [{name: string, year: number}] = [{
     name: 'Ford',
