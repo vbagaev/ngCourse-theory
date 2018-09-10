0441c3627a05bce2b760de73f3808b7094796336
Valeriy_V_Bagaev <v.bagaev@sarrc.ru>
2018-09-10 06:50:42 +0100

    sevice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { CarsComponent } from './cars/cars.component';
-import { CarComponent } from './car/car.component';
-import { AddCarComponent } from './add-car/add-car.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CarsComponent,
-    CarComponent,
-    AddCarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
