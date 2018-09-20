2c0c791e83fb66c4bd0f6b01b849829b7574decb
Valeriy_V_Bagaev <v.bagaev@sarrc.ru>
2018-09-20 10:53:49 +0400

    R Создание валидатора

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,21 +15,31 @@ export class AppComponent implements OnInit {
     text: 'Нет'
   }];
 
+  charsCount = 5;
+
   ngOnInit() {
   this.form = new FormGroup({
     user: new FormGroup({
       email: new FormControl('ii', [Validators.required, Validators.email]),
-      pass: new FormControl('', Validators.required),
+      pass: new FormControl('', [Validators.required, this.checkForLength.bind(this)]),
     }),
     country: new FormControl('ru'),
     answer: new FormControl('no')
-  })
+  });
   }
 
   form: FormGroup;
 
   onSubmit() {
     console.log('submited!!!', this.form);
-  }
+  };
 
+  checkForLength(control: FormControl) {
+    if (control.value.length <= this.charsCount) {
+      return {
+        'lengthError': true
+      };
+    }
+    return null;
+  }
 }
