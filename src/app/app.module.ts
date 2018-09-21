63b894b18bf9e80ce777363e45c859df69bc460f
Valeriy_V_Bagaev <v.bagaev@sarrc.ru>
2018-09-21 13:42:58 +0400

    Создание роутинг модуля

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { CarsPageComponent } from './cars-page/cars-page.component';
 import { CarsService } from './cars.service';
-import {Routes, RouterModule} from '@angular/router';
-
-const appRoutes: Routes = [
-  {
-    path: 'cars',
-    component: CarsPageComponent
-  },
-  {
-    path: '',
-    component: HomePageComponent
-  }
-];
-
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -31,9 +18,10 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [CarsService],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
